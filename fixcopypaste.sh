c648d3e1c6ee6df0af6d8741705c9bb8ae464697
terrence brannon <metaperl@lifehost.launchmodem.com>
2010-03-10 16:11:46 -0500

    autocutsel fix

---
+++ fixcopypaste.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+
+ autocutsel -selection PRIMARY &
+ autocutsel -selection CLIPBOARD &
+
+# http://www.livingcosmos.org/2010/03/virtualbox-copypaste-with-windows-host-and-linux-guest/
