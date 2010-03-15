b24a73cea4afdb91eaf801bbdaae95131ea8a105
Terrence Brannon <metaperl@locohost.(none)>
2010-03-15 17:42:57 -0400

    myssh refactoring

---
+++ lib/perl5/MySSH.pm
@@ -0,0 +1,12 @@
+package MySSH;
+
+our %user = (
+	     'li2-168.members.linode.com'   => 'schemelab',
+	     'natasha.regents.state.oh.us'  => 'tbrannon',
+
+	     'lnc.usc.edu'                 => 'brannon',
+	     'perlmonk.org'                => 'metaperl',
+	    ) ;
+
+
+1;
