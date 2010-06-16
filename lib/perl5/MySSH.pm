9f52a29b24cc15b19ecab726a4ebf4d5e382e7a9
U-LIFEBOOK\Administrator <Administrator@LIFEBOOK.(none)>
2010-06-16 18:44:56 -0400

    fix

--- lib/perl5/MySSH.pm
+++ lib/perl5/MySSH.pm
@@ -1,11 +1,19 @@
 package MySSH;
 
 our %user = (
-	     'li2-168.members.linode.com'   => 'schemelab',
-	     'natasha.regents.state.oh.us'  => 'tbrannon',
 
-	     'lnc.usc.edu'                 => 'brannon',
-	     'perlmonk.org'                => 'metaperl',
+	     'martin.opbdev.net'        => 'root',
+	     'zuul.boca.verio.net'      => 'tbrannon',
+	     'dev2.boca.verio.net'      => 'tbrannon',
+	     'nm.vwh.net'               => 'root',
+	     'cvs.vwh.net'              => 'tbrannon',
+	     'jump0.wh.verio.net'       => 't.brannon',
+	     'txn95.boca95-verio.com'   => 'root',
+
+
+	     'sd03.securesites.net'         => 'root',
+	     'li2-168.members.linode.com'   => 'schemelab',
+
 	    ) ;
 
 
