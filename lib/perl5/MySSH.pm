4a5b8b9e2dbd65243f14a87a84b56e25bda64553
Terrence Brannon <thequietcenter@ubuntu.(none)>
2010-10-14 07:04:54 -0400

    fixes

--- lib/perl5/MySSH.pm
+++ lib/perl5/MySSH.pm
@@ -2,17 +2,8 @@ package MySSH;
 
 our %user = (
 
-	     'martin.opbdev.net'        => 'root',
-	     'zuul.boca.verio.net'      => 'tbrannon',
-	     'dev2.boca.verio.net'      => 'tbrannon',
-	     'nm.vwh.net'               => 'root',
-	     'cvs.vwh.net'              => 'tbrannon',
-	     'jump0.wh.verio.net'       => 't.brannon',
-	     'txn95.boca95-verio.com'   => 'root',
-
-
-	     'sd03.securesites.net'         => 'root',
 	     'li2-168.members.linode.com'   => 'schemelab',
+	     'ec2-174-129-147-253.compute-1.amazonaws.com'   => 'ubuntu',
 
 	    ) ;
 
