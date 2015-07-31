a177bb0993fc0bb21d34df54080ee6da7f1f4bfb
Terrence Brannon <metaperl@gmail.com>
2015-07-31 11:18:51 -0700

    xlutils xlwt xlrd

--- copy-rename/cr.py
+++ copy-rename/cr.py
@@ -3,8 +3,17 @@
 import  shutil
 import datetime
 
+def new_filename():
+    return 'timesheet-{0}.xls'.format(datetime.date.today().isoformat())
 
 
 shutil.copy(
-    'default.xls',
-    'timesheet-{0}.xls'.format(datetime.date.today().isoformat()))
+    'default.xls', new_filename())
+
+from xlrd import open_workbook
+from xlutils.copy import copy
+
+rb = open_workbook(new_filename())
+wb = copy(rb)
+s = wb.get_sheet(0)
+print s
